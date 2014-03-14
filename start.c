f11072169cb2f247bc05f8a6826266f079158aa9
yl68 <liyijie714@gmail.com>
2014-03-13 21:31:05 -0500

    fix errors

--- start.c
+++ start.c
@@ -1,15 +1,15 @@
 #include<stdlib.h>
 
-#include<hardware.h>
-#include<yalnix.h>
+#include "hardware.h"
+#include "yalnix.h"
 
 void *brk;
 int head;
-void KernelStart(ExceptionStackFrame *frame, Unsigned int pmem_size, void *org_brk, char **cmd_args)
+void KernelStart(ExceptionStackFrame *frame, unsigned int pmem_size, void *org_brk, char **cmd_args)
 {
 	int i,addr;
 	
-	void (*vector_table[TRAP_VEACTOR_SIZE])(ExceptionStackFrame *);
+	void (*vector_table[TRAP_VECTOR_SIZE])(ExceptionStackFrame *);
 	struct pte *pt1,*pt2;
 	brk=org_brk;
 	for ( i=0;i<TRAP_VECTOR_SIZE;i++)
@@ -18,9 +18,9 @@ void KernelStart(ExceptionStackFrame *frame, Unsigned int pmem_size, void *org_b
 	WriteRegister(VMEM_BASE,(RCS421RegVal)PMEM_BASE);
 	WriteRegister(VMEM_0_BASE,(RCS421RegVal)PMEM_BASE);
 	WriteRegister(VMEM_SIZE,(RCS421RegVal)pmem_size);
-	WriteRegister(VMEM_0_LIMI,(RCS421RegVal)(PMEM_BASE+pmem_size)/2);
+	WriteRegister(VMEM_0_LIMIT,(RCS421RegVal)(PMEM_BASE+pmem_size)/2);
 	WriteRegister(VMEM_1_BASE,(RCS421RegVal)(PMEM_BASE+pmem_size)/2);
-	WriteRegister(VMEM_1_LIMI,(RCS421RegVal)(PMEM_BASE+pmem_size));
+	WriteRegister(VMEM_1_LIMIT,(RCS421RegVal)(PMEM_BASE+pmem_size));
 	addr=0;
 	pt1=(struct pte *)malloc(PAGE_TABLE_LEN * sizeof(struct pte));
 	pt2=(struct pte *)malloc(PAGE_TABLE_LEN * sizeof(struct pte));
@@ -31,7 +31,7 @@ void KernelStart(ExceptionStackFrame *frame, Unsigned int pmem_size, void *org_b
 		if(((i*PAGESIZE) > KERNEL_STACK_BASE && (i*PAGESIZE <brk))||(((i+1)*PAGESIZE-1 )> KERNEL_STACK_BASE && ((i+1)*PAGESIZE -1 <brk)))
 			continue;
 		*(int *)addr=(i*PAGESIZE);
-		addr=(i*PAGE_SIZE);
+		addr=(i*PAGESIZE);
 			
 	}
 
